871db1a45b799625d5eb436faaabc4c04b65a39f
unknown <nur.mnzaman@gmail.com>
2023-09-02 16:31:28 +0600

    [feat] fetch data from todos json api

--- fetchDataUsingReduxToolkit/src/features/todosData/TodosSlice.jsx
+++ fetchDataUsingReduxToolkit/src/features/todosData/TodosSlice.jsx
@@ -1,36 +1,35 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchDatas = createAsyncThunk ("TodosData/fetchData", async()=>{
-    const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+export const fetchDatas = createAsyncThunk("TodosData/fetchData", async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+  return res.data;
+});
 
-    return res.data
-})
+const todosSlice = createSlice({
+  name: "todosData",
+  initialState: {
+    todos: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchDatas.pending, (state) => {
+      state.isLoading = true;
+    });
 
-const todosSlice = createSlice ({
-    name : "todosData",
-    initialState : {
-        todos : [],
-        isLoading : false,
-        error : null
-    },
-    extraReducers : (builder) => {
-        builder.addCase(fetchDatas.pending, (state) => {
-            state.isLoading = true;
-        })
+    builder.addCase(fetchDatas.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.todos = action.payload;
+      state.error = null;
+    });
 
-        builder.addCase(fetchDatas.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.todos = action.payload;
-            state.error = null
-        })
+    builder.addCase(fetchDatas.rejected, (state, action) => {
+      state.isLoading = false;
+      state.todos = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
-        builder.addCase(fetchDatas.rejected, (state, action) => {
-            state.isLoading = false;
-            state.todos = []
-            state.error = action.error.message
-        })
-    }
-})
-
-export default todosSlice.reducer
+export default todosSlice.reducer;
--- fetchDataUsingReduxToolkit/src/features/todosData/TodosView.jsx
+++ fetchDataUsingReduxToolkit/src/features/todosData/TodosView.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDatas } from "./TodosSlice";
 
 const Todos = () => {
-    const {todos, isLoading, error} = useSelector ( state => state.todos)
+  const { todos, isLoading, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-    console.log(todos);
+  useEffect(() => {
+    dispatch(fetchDatas());
+  }, []);
+
   return (
     <div>
-        <section>
-            <h1> Todos App - Fetching data from api</h1>
-        </section>
-
+      <section>
+        <h1> Todos App - Fetching data from api</h1>
+        {isLoading && <p>isLoading...</p>}
+        {error && <p>{error}</p>}
+        {todos &&
+          todos.map((todo) => {
+            return (
+              <article key={todo.id} className="card">
+                <p> {todo.id} </p>
+                <p> {todo.title} </p>
+              </article>
+            );
+          })}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
